471c1acaf75c3926d8a55c18ab6b0fc21daa0cfb
Tushar <tusharofcl@gmail.com>
2023-12-26 17:26:34 +0530

    feat:react_with_redux_all_concepts

---
+++ src/features/iceCream/IcecreamView.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { ordered, restocked } from "./IcecreamSlice";
+
+const IcecreamView = () => {
+  const numOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h2>Number of Icecreams-{numOfIcecreams}</h2>
+      <button onClick={() => dispatch(ordered())} type="">
+        Order Icecream
+      </button>
+      <button onClick={() => dispatch(restocked(45))} type="">
+        Restock Icecreams
+      </button>
+    </div>
+  );
+};
+
+export default IcecreamView;
